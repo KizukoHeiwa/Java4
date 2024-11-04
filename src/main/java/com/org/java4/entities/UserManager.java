7dce70317b167723371b8c5e1fad27dd249cea4d
Admin <h.thuy7605@gmail.com>
2024-11-04 15:27:44 +0700

    Update master

--- src/main/java/com/org/java4/entities/UserManager.java
+++ src/main/java/com/org/java4/entities/UserManager.java
@@ -24,15 +24,15 @@ public class UserManager {
         });
     }
 
-    public void findById() {
-        User user = em.find(User.class, "<<user-id>>");
+    public void findById(String id) {
+        User user = em.find(User.class, id);
         String fullname = user.getFullname();
         boolean admin = user.getAdmin();
         System.out.println(fullname + ":" + admin);
     }
 
     public void create() {
-        User user = new User("U01", "123", "teo@gmail.com", "Tèo", false);
+        User user = new User("U06", "123", "teo@gmail.com", "Tèo", false);
         try {
             em.getTransaction().begin();
             em.persist(user);
@@ -43,7 +43,7 @@ public class UserManager {
     }
 
     public void update() {
-        User user = em.find(User.class, "U01");
+        User user = em.find(User.class, "U06");
         user.setFullname("Nguyễn Văn Tèo");
         user.setEmail("teonv@gmail.com");
 
@@ -57,7 +57,7 @@ public class UserManager {
     }
 
     public void deleteById() {
-        User user = em.find(User.class, "U01");
+        User user = em.find(User.class, "U06");
         try {
             em.getTransaction().begin();
             em.remove(user);
@@ -65,6 +65,34 @@ public class UserManager {
         } catch (Exception e) {
             em.getTransaction().rollback();
         }
+    }
+
+    public void findWhere() {
+        String jpql = "SELECT o FROM User o WHERE o.email LIKE :search AND o.admin=:role";
+        TypedQuery<User> query = em.createQuery(jpql, User.class);
+        query.setParameter("search", "%@fpt.edu.vn");
+        query.setParameter("role", false);
+        List<User> list = query.getResultList();
+
+        list.forEach(user -> {
+            String fullname = user.getFullname();
+            boolean admin = user.getAdmin();
+            String email = user.getEmail();
+            System.out.println(fullname + ": " + admin + " - " + email);
+        });
+    }
+
+    public List<User> findWithPage(int pageNumber, int pageSize) {
+        TypedQuery<User> query = em.createQuery("SELECT o FROM User o", User.class);
+
+        // Đặt vị trí bắt đầu cho phân trang (pageNumber tính từ 0)
+        query.setFirstResult(pageNumber * pageSize);
+
+        // Đặt số lượng kết quả tối đa mỗi lần truy vấn (pageSize)
+        query.setMaxResults(pageSize);
+        List<User> list = query.getResultList();
+
+        return list;
     }
 }
 
