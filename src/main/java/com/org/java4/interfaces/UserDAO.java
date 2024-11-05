9d3415aaa9a7d508e59fb80df58acbed28e05675
Hoang Thuy - Desktop <h.thuy7605@gmail.com>
2024-11-06 01:07:32 +0700

    Update master

---
+++ src/main/java/com/org/java4/interfaces/UserDAO.java
@@ -0,0 +1,18 @@
+package com.org.java4.interfaces;
+
+import com.org.java4.model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    /**Truy vấn tất cả*/
+    List<User> findAll();
+    /**Truy vấn theo mã*/
+    User findById(String id);
+    /**Thêm mới*/
+    void create(User item);
+    /**Cập nhật*/
+    void update(User item);
+    /**Xóa theo mã*/
+    void deleteById(String id);
+}
